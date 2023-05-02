d44a7f73e035edcb06572caa6068361d54c8bce3
juneil-park <131658338+juneil-park@users.noreply.github.com>
2023-05-03 08:35:34 +0900

    Update problem1_matrix.cpp
    - 예외처리 추가
    - 코딩표준에 맞게 변수 이름 변경

--- problem1_matrix.cpp
+++ problem1_matrix.cpp
@@ -3,66 +3,68 @@
 #include <random>
 class Matrix
 {
-private:
-    int mColumb;
-    int mRow;
-    std::vector<std::vector<int>> mMat;
-
 public:
+    Matrix(int i, int j); // vector를 주지 않으면 0~10사이에서 랜덤생성
+    Matrix(int i, int j, std::vector<std::vector<int>>); // contructor 는 가장 위에 사용
+
     int GetNumberOfRow();
     int GetNumberOfColumn();
     void SetElement(int i, int j, int value);
     void Display();
     Matrix AddMatrices(Matrix B);
     Matrix MultiplyMatrices(Matrix B);
-    Matrix(int i, int j); // vector를 주지 않으면 0~10사이에서 랜덤생성
-    Matrix(int i, int j, std::vector<std::vector<int>>);
+ 
+private: // private 변수는 뒤에 언더바 _ 
+    int Columb_;
+    int Row_;
+    std::vector<std::vector<int>> Mat_;
+
 
 };
 
 
 Matrix::Matrix(int i, int j) //vector를 주지 않으면 0~10사이에서 랜덤생성
-: mRow(i), mColumb(j) 
+: Row_(i), Columb_(j) 
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(0,10); //0~10에서 난수 생성
-    std::vector<std::vector<int>> vec_2d(mRow, std::vector<int>(mColumb,0));
+    std::vector<std::vector<int>> vec_2d(Row_, std::vector<int>(Columb_,0));
 
-    for (int i =0; i< mRow; i++)
+    for (int i =0; i< Row_; i++)
     {
-        for (int j=0; j<mColumb; j++)
+        for (int j=0; j<Columb_; j++)
         {
             vec_2d[i][j] = dis(gen);
         }
     }
-    mMat = vec_2d;
+    Mat_ = vec_2d;
 }
 
 Matrix::Matrix(int i, int j, std::vector<std::vector<int>> mat) //생성자
-: mRow(i), mColumb(j), mMat(mat){}
+: Row_(i), Columb_(j), Mat_(mat){} // initialization
 
 int Matrix::GetNumberOfColumn()
 {
-    return mColumb;
+    return Columb_;
 }
 
 int Matrix::GetNumberOfRow()
 {
-    return mRow;
+    return Row_;
 }
 
 void Matrix::SetElement(int i, int j, int value)
 {
-    mMat[i][j] = value;
+    Mat_[i][j] = value;
 }
 
 void Matrix::Display()
 {
-    for (int i=0; i< mRow; i++)
-        {for (int j=0; j< mColumb; j++)
+    for (int i=0; i< Row_; i++)
+        {for (int j=0; j< Columb_; j++)
             {
-                std::cout <<  mMat[i][j] << " ";
+                std::cout <<  Mat_[i][j] << " ";
             }
             std::cout << "\n";
         }
@@ -70,53 +72,49 @@ void Matrix::Display()
 
 Matrix Matrix::AddMatrices(Matrix B)
 {
-    if ((mRow == B.mRow) && (mColumb == B.mColumb))
+    if ((Row_ == B.Row_) && (Columb_ == B.Columb_))
     {
-        std::vector<std::vector<int>> temp(mRow, std::vector<int>(mColumb,0));
+        std::vector<std::vector<int>> temp(Row_, std::vector<int>(Columb_,0));
         
-        for (int i =0; i< mRow; i++)
+        for (int i =0; i< Row_; i++)
         {
-            for (int j=0; j<mColumb; j++)
+            for (int j=0; j<Columb_; j++)
             {
-                temp[i][j] = mMat[i][j] + B.mMat[i][j];
+                temp[i][j] = Mat_[i][j] + B.Mat_[i][j];
             }
         }
 
-        return Matrix(mRow,mColumb,temp);
+        return Matrix(Row_,Columb_,temp);
         
     }
-    std::cout << "Add error" << std::endl;
-        
-    return Matrix(mRow,mColumb,mMat); // 더 좋은 방법이 있을듯? 에러 났을때 리턴을 안주는 방법??
-    
+
+    throw std::string("Add error. 두 행렬의 차원이 덧셈연산에 적합하지 않습니다."); //예외처리
     
 }
 
 
 Matrix Matrix::MultiplyMatrices(Matrix B)
 {
-    if (mColumb == B.mRow)
+    if (Columb_ == B.Row_)
     {
-        std::vector<std::vector<int>> temp(mRow, std::vector<int>(mColumb,0));
+        std::vector<std::vector<int>> temp(Row_, std::vector<int>(B.Columb_,0));
 
-        for (int i =0; i< mRow; i++)
+        for (int i =0; i< Row_; i++)
         {
-            for (int j=0; j<mColumb; j++)
+            for (int j=0; j<B.Columb_; j++)
             {
                 // temp[i][j] = 0;
-                for (int k=0 ; k<mColumb; k++)
+                for (int k=0 ; k<Columb_; k++)
                 {
-                    temp[i][j] += (mMat[i][k] *B.mMat[k][j]);
+                    temp[i][j] += (Mat_[i][k] *B.Mat_[k][j]);
                 } 
             }
         }
 
-        return Matrix(mColumb,mRow,temp);
+        return Matrix(Row_,B.Columb_,temp);
     }
 
-    std::cout << "Multiply error" << std::endl;
-        
-    return Matrix(mRow,mColumb,mMat); // 더 좋은 방법이 있을듯?
+    throw std::string("Multiply Error. 두 행렬의 차원이 곱셈연산에 적합하지 않습니다."); ////예외처리
 }
 
 
@@ -142,9 +140,19 @@ int main()
     // Matrix matrix2(3,3,m2);
     matrix1.Display();
     matrix2.Display();
+    try
+    {
     Matrix a = matrix1.AddMatrices(matrix2);
     a.Display();
+    } catch (std::string& s){
+        std::cout << "Error : " << s << std:: endl; ////예외처리
+    }
+    try{
     Matrix b = matrix1.MultiplyMatrices(matrix2);
     b.Display();
+    } catch (std::string& s){
+        std::cout << "Error : " << s << std::endl; ////예외처리
+    }
+    
     return 0;
 }  
